27b671ea8cfd20556a27e86629df4438c8834895
01111111111111111111111111111111 <32463675+01111111111111111111111111111111@users.noreply.github.com>
2017-10-10 15:39:48 -0400

    GuessGame project--game object with three player objects--rand num, try to guess

---
+++ GuessGame/src/GameLauncher.java
@@ -0,0 +1,10 @@
+/**
+ * Created by mwz16 on 10/10/2017.
+ */
+public class GameLauncher {
+    public static void main (String[] args){
+
+        GuessGame game = new GuessGame();
+        game.startGame();
+    }
+}
